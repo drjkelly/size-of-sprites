26f0eba9b157092e1105abc76e911cb6ea5c6892
drjkelly <92182168+drjkelly@users.noreply.github.com>
2021-10-11 20:25:51 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,29 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "pizza":return img`
+4 3 3 3 4 . . . 
+4 2 4 2 4 3 4 . 
+4 4 4 4 2 4 2 . 
+4 4 2 4 4 4 . . 
+4 4 4 4 4 . . . 
+4 2 4 4 . . . . 
+4 4 . . . . . . 
+4 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
